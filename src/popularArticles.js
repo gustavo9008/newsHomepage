9a2937af6ae4f572748c11bcb1b69ac42f62e05f
Gustavo Ayala <gustavo.ayala7@gmail.com>
2023-01-17 17:24:50 -0800

    add links to titles in popular articles section

--- src/popularArticles.js
+++ src/popularArticles.js
@@ -3,7 +3,7 @@ document.querySelector('#popularArticles').innerHTML = `
 <img class="imgRetro" src="/image-retro-pcs.jpg" alt="">
 <div class="popularInfo">
 <h4>01</h4>
-<h5>Reviving Retro PCs</h5>
+<h5><a href="#">Reviving Retro PCs</a></h5>
 <p>What happens when old PCs are given modern upgrades?</p>
 </div>
 
@@ -12,7 +12,7 @@ document.querySelector('#popularArticles').innerHTML = `
 <img class="imgRetro" src="/image-top-laptops.jpg" alt="">
 <div class="popularInfo">
 <h4>02</h4>
-<h5>Top 10 Laptops of 2022</h5>
+<h5><a href="#">Top 10 Laptops of 2022</a></h5>
 <p>Our best picks for various needs and budgets. </p>
 </div>
 
@@ -21,7 +21,7 @@ document.querySelector('#popularArticles').innerHTML = `
 <img class="imgRetro" src="/image-gaming-growth.jpg" alt="">
 <div class="popularInfo">
 <h4>03</h4>
-<h5>The Growth of Gaming</h5>
+<h5><a href="#">The Growth of Gaming</a></h5>
 <p>How the pandemic has sparked fresh opportunities.</p>
 </div>
 
